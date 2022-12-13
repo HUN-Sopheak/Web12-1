41808eb2dd263ca19727b5fbc6172795e2b93cfa
HUN-Sopheak <sopheakhun030@gmail.com>
2022-12-13 13:03:11 +0700

    first commit

---
+++ 04.js/setAttr.js
@@ -0,0 +1,6 @@
+const btn_change=()=>{
+     img=document.getElementById("IMG");
+     text=document.querySelector("font");
+    img.setAttribute("src","/Assets/img/2.gif");
+    text.setAttribute("color","blue");
+}
