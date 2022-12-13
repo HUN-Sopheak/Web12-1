4d685383fadbf9bfb5cfb1d9ce40be16bfaa1f26
HUN-Sopheak <sopheakhun030@gmail.com>
2022-12-13 13:52:50 +0700

    first commit

--- 04.js/style.js
+++ 04.js/style.js
@@ -3,3 +3,21 @@ event1.addEventListener('click',()=>{
     var box =document.querySelector(".box");
     box.style.backgroundColor="red";
 });
+const hide=()=>{
+    event1.style.display="none";
+}
+const show=()=>{
+    event1.style.display="block";
+}
+i=true;
+const toggle=()=>{
+    if(i){
+        event1.style.display="none";
+       document.getElementById("tt").innerHTML="show";
+    }else{
+        event1.style.display="block";
+       document.getElementById("tt").innerHTML="hide";
+
+    }
+    i=!i;
+}
