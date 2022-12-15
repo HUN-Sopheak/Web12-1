6a9ea27585cfed6fb4fdb62cfc3e40c91d08f50f
HUN-Sopheak <sopheakhun030@gmail.com>
2022-12-15 13:52:29 +0700

    first commit

--- 04.js/test.js
+++ 04.js/test.js
@@ -1,4 +1,18 @@
-// lastName="Clint";
-// firstName="Alomnd";
-// console.log(firstName+"\t"+lastName);
- 
+var a = 10;
+var b=20;
+if(a>b){
+    console.log("A>B")
+}else{
+    console.log("A<B")
+}
+(a>b) ? console.log("A>B") : console.log("A<B")
+//do while
+//while
+//for
+//forin
+//forof
+//foreach()
+//map()
+//Array
+//Array Object
+//end
